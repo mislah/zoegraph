a9d98c0c3d7227c221001345a814bb1e5d5d5585
Mislah <git@mislah.com>
2023-10-08 08:41:34 +0530

    Add style and update time format
    Added a stylesheet to set fontsize and adjust opacity of the overlay
    clock. The offset has been adjusted for better user experience. Also,
    updated the clock to show 12h format instead of 24h format.

--- zoegraph@ext.mislah.com/extension.js
+++ zoegraph@ext.mislah.com/extension.js
@@ -2,14 +2,14 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 
-const offset = { x: -25, y: -25 }
+const offset = { x: -50, y: -33 }
 
 class Extension {
     #disp;
     #timeout;
 
     enable() {
-        this.#disp = new St.Label({ text: "00:00:00" });
+        this.#disp = new St.Label({ style_class: 'main', text: "00:00:00" });
         this.#disp.set_position(
             Main.layoutManager.primaryMonitor.width - this.#disp.width + offset.x,
             Main.layoutManager.primaryMonitor.height - this.#disp.height + offset.y,
@@ -17,7 +17,7 @@ class Extension {
         Main.uiGroup.add_actor(this.#disp);
         this.#timeout = Mainloop.timeout_add_seconds(1, () => {
             let date = new Date();
-            this.#disp.text = date.toLocaleFormat("%H:%M:%S");
+            this.#disp.text = date.toLocaleFormat("%I:%M:%S");
             return true;
         });
     }
