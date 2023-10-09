178a11f14146bc06af2b9a966bea251235c8ae91
Mislah <git@mislah.com>
2023-10-09 17:25:18 +0530

    Add zoegraph (long term chronograph)
    Zoegraph displays millisecond precision chronograph in years. It takes
    a start date with time and displays the elapsed time in years since
    then. It can be used to display precise age or time since an event.

--- zoegraph@ext.mislah.com/extension.js
+++ zoegraph@ext.mislah.com/extension.js
@@ -2,7 +2,8 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 
-const offset = { x: -50, y: -33 }
+// const offset = { x: -50, y: -33 }
+const offset = { x: -200, y: -33 }
 
 class Extension {
     #disp;
@@ -15,9 +16,25 @@ class Extension {
             Main.layoutManager.primaryMonitor.height - this.#disp.height + offset.y,
         );
         Main.uiGroup.add_actor(this.#disp);
+
+        // this.#clock();
+        this.#zoegraph("2000-01-01");
+    }
+
+    #clock() {
         this.#timeout = Mainloop.timeout_add_seconds(1, () => {
             let date = new Date();
             this.#disp.text = date.toLocaleFormat("%I:%M:%S");
+            return true;
+        });
+    }
+
+    #zoegraph(startDateWithTime) {
+        const startTime = new Date(startDateWithTime).getTime();
+        this.#timeout = Mainloop.timeout_add(1, () => {
+            let currentTime = new Date().getTime();
+            let elapsedTime = ((currentTime - startTime) / 1000) / (365.25 * 24 * 60 * 60);
+            this.#disp.text = elapsedTime.toString();
             return true;
         });
     }
