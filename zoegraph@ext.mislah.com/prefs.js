dee60eac9645f9ee4126f57529de67ca855534d9
Mislah <git@mislah.com>
2023-10-23 15:47:56 +0530

    Add preference window
    Introduced a preference window for modifying the default settings of the
    extension. The layout of the preference window is defined in settings.ui.
    The preference window can be accessed through gnome-extension manager or
    through the command line. Any changes made in the preference window become
    visible after restarting the shell, and they persist across sessions.

---
+++ zoegraph@ext.mislah.com/prefs.js
@@ -0,0 +1,78 @@
+const ExtensionUtils = imports.misc.extensionUtils;
+const Self = ExtensionUtils.getCurrentExtension();
+const GObject = imports.gi.GObject;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+
+const PrefsWidget = GObject.registerClass({
+    GTypeName: 'PrefsWidgets',
+    Template: Self.dir.get_child('settings.ui').get_uri(),
+    InternalChildren: [
+        'c_off_x',
+        'c_off_y',
+        'z_off_x',
+        'z_off_y',
+        'clock_24h',
+        'default_mode',
+        'zoegraph_date',
+    ],
+}, class PrefsWidget extends Gtk.Box {
+
+    _init(params = {}) {
+        super._init(params);
+        this._settings = ExtensionUtils.getSettings(Self.metadata['settings-schema']);
+        this._bind('offset-clock-x', 'c_off_x', 'value');
+        this._bind('offset-clock-y', 'c_off_y', 'value');
+        this._bind('offset-zoegraph-x', 'z_off_x', 'value');
+        this._bind('offset-zoegraph-y', 'z_off_y', 'value');
+        this._bind('clock-24h', 'clock_24h', 'true');
+        
+        const defaultMode = this._widget('default_mode');
+        defaultMode.set_active(this._settings.get_enum('mode'));
+        defaultMode.connect('changed', (combobox) => {
+            this._settings.set_enum('mode', combobox.get_active());
+        });
+
+        const zoeDate = this._widget('zoegraph_date');
+        zoeDate.set_text(this._settings.get_string('zoegraph-anchor-date'));
+        zoeDate.connect('changed', (entry) => {
+            this._settings.set_string('zoegraph-anchor-date', entry.get_text());
+        });
+    }
+
+    _widget(id) {
+        const name = '_' + id;
+        if (!this[name]) {
+            throw `Unknown widget with ID "${id}"!`;
+        }
+
+        return this[name];
+    }
+
+    _bind(settingsKey, widgetId, widgetProperty, flag = Gio.SettingsBindFlags.DEFAULT) {
+        const widget = this._widget(widgetId);
+        this._settings.bind(settingsKey, widget, widgetProperty, flag);
+        this._settings.bind_writable(settingsKey, widget, 'sensitive', false);
+    }
+
+    _bindWidgetSensitive(widgetId, settingsKey, invert = false) {
+        this._settings.connect(
+            'changed::' + settingsKey,
+            () => this._updateWidgetSensitive(widgetId, settingsKey, invert),
+        );
+        this._updateWidgetSensitive(widgetId, settingsKey, invert);
+    }
+
+    _updateWidgetSensitive(widgetId, settingsKey, invert) {
+        const active = this._settings.get_boolean(settingsKey);
+        this._widget(widgetId).set_sensitive(invert ? !active : active);
+    }
+});
+
+function init() {
+    ExtensionUtils.initTranslations('zoegraph');
+}
+
+function buildPrefsWidget() {
+    return new PrefsWidget();
+}
